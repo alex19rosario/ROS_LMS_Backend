b0b7ee88798aafa5a41c89d142658914ce068f75
Carlos Rosario <crosari5@my.centennialcollege.ca>
2025-11-06 14:40:01 -0500

    Fixing config class

--- src/main/java/com/ros/lms/infraestructure/config/DynamoDbConfig.java
+++ src/main/java/com/ros/lms/infraestructure/config/DynamoDbConfig.java
@@ -1,8 +1,11 @@
 package com.ros.lms.infraestructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -11,10 +14,24 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 @Profile("prod")
 public class DynamoDbConfig {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.US_EAST_1)
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
                 .build();
     }
 
